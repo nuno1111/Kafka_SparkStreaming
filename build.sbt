7dfd5e1f62146efb04553422c5f3ba5ad4508140
test <dh1026han@nice.co.kr>
2018-07-06 15:05:49 +0900

    Done, to ElasticSearch

--- build.sbt
+++ build.sbt
@@ -4,6 +4,14 @@ version := "0.1"
 
 scalaVersion := "2.11.12"
 
-libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.1.2"
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.2"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.2" % "provided"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.1.2" % "provided"
+libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.1.2"
+//libraryDependencies += "org.elasticsearch" %% "elasticsearch-spark" % "2.1.2"
+libraryDependencies += "org.scalaj" % "scalaj-http_2.11" % "2.3.0"
 
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
--- src/main/scala/SystemLogConsumerStreaming.scala
+++ src/main/scala/SystemLogConsumerStreaming.scala
@@ -1,16 +1,68 @@
 
 import org.apache.spark._
 import org.apache.spark.streaming._
-//import org.apache.spark.streaming.StreamingContext._ // not necessary since Spark 1.3
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import scalaj.http._
+import java.util.Date
 
 object SystemLogConsumerStreaming {
   def main(args: Array[String]): Unit = {
 
-    // Create a local StreamingContext with two working thread and batch interval of 1 second.
-    // The master requires 2 cores to prevent a starvation scenario.
+    val conf = new SparkConf().setMaster("local[2]").setAppName("SystemLogConsumerStreaming")
 
-    val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
     val ssc = new StreamingContext(conf, Seconds(1))
+
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "localhost:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "SparkStreaming-SytemLog-ConsumerGroup",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (true: java.lang.Boolean)
+    )
+
+    val topics = Array("peter-log")
+    val stream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+
+
+
+    val format = new java.text.SimpleDateFormat("yyyy-MM-dd_HH:mm:ss.SSS")
+    val today = new java.text.SimpleDateFormat("yyyy-MM-dd")
+    stream.foreachRDD { rdd =>
+      if( !rdd.isEmpty ) {
+
+        val date = new Date()
+
+        rdd.map(_.value).foreach { param =>
+          val res = Http("http://localhost:9200/peter-log-" + today.format(date) + "/systemlog").postData(param)
+            .header("Content-Type", "application/json")
+            .header("Charset", "UTF-8")
+            .option(HttpOptions.readTimeout(10000)).asString
+
+
+//          println("==============================================================================================")
+//          println(res)
+//          println("==============================================================================================")
+        }
+
+        rdd.map(_.value).saveAsTextFile("/home/devuser/data/systemlog-"+format.format(date))
+
+
+
+      }
+    }
+
+    println("==============================================================================================")
+
+    ssc.start()             // Start the computation
+    ssc.awaitTermination()  // Wait for the computation to terminate
 
   }
 }
